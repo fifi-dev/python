4fe361ec965eb0a062ccf1c6e43dcd7878c2f9d9
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2022-03-22 22:04:27 +0100

    dynamic url & parse html

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ requests_cache.install_cache('demo_cache')
 # On demande à l'utilisateur de rentrer sa ville
 
 ville = input("Quel est votre ville ").strip()
+
+# url dynamique vers notre page web
+urlpage = 'https://www.google.com/search?q=meteo+{ville}'
+
+# query the website and return the html to the variable 'page'
+page = requests.get(urlpage).text
+# On parse le html en utilisant beautiful soup et on le stocke dans la variable 'soup'
+soup = BeautifulSoup(page, 'html.parser')
